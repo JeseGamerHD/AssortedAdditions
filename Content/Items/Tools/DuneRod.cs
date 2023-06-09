ec881d02159672295661a54d1ca62301e338e8bc
JeseGamerHD <->
2023-06-09 13:56:11 +0300

    Added Dune Rod + Bobber

---
+++ Content/Items/Tools/DuneRod.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using ModdingTutorial.Content.Items.Placeables;
+
+namespace ModdingTutorial.Content.Items.Tools;
+
+public class DuneRod : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.WoodFishingPole); // Most defaults can be copied
+        Item.value = Item.sellPrice(silver: 30);
+        Item.fishingPole = 35; // Fishing power
+        Item.shootSpeed = 14f; // Speed at which the bobbers are launched.
+        
+        Item.shoot = ModContent.ProjectileType<Projectiles.DuneRodBobber>(); // Has an unique bobber
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ModContent.ItemType<DuneBar>(), 8);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+    }
+}
---
+++ Content/Projectiles/DuneRodBobber.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Projectiles;
+
+public class DuneRodBobber : ModProjectile // Bobber for the Dune Rod item
+{
+    public override void SetDefaults()
+    {
+        Projectile.CloneDefaults(ProjectileID.BobberWooden);
+        DrawOriginOffsetY = -8; // So that 
+    }
+
+    public override void ModifyFishingLine(ref Vector2 lineOriginOffset, ref Color lineColor)
+    {
+        lineOriginOffset = new Vector2(44, -29); // Where the line is drawn from
+        lineColor = Color.LightGray; // Sets the fishing line's color.
+    }
+}
