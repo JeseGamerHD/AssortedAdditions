2a56c44637789da2f24f1c77975816349beda5c7
JeseGamerHD <->
2023-06-13 10:34:54 +0300

    Further fixes to Dragon Staff - almost done

--- Content/Items/Weapons/Summon/DragonStaff.cs
+++ Content/Items/Weapons/Summon/DragonStaff.cs
@@ -39,24 +39,40 @@ public class DragonStaff : ModItem
         position = Main.MouseWorld; // Spawns at cursor position
     }
 
+
+
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
-        for(int i = 0; i < Main.projectile.Length; i++)
+        for (int i = 0; i < Main.projectile.Length; i++) // Looking through player's summons
         {
             Projectile proj = Main.projectile[i];
             if (proj.active && proj.type == ModContent.ProjectileType<DragonMinionProj>() && proj.owner == player.whoAmI)
             {  // Only one of this minion can exist
                // This section despawns the first minion when summoning another
-                proj.active = false; // Without this the new minion won't spawn at the cursor, but at the previous minion's location
+                proj.active = false;
+                break;
+            }
+
+            // If a different minion exists it will also be replaced by the dragon if no slots are available
+            // If there are slots dragon will spawn without replacing/removing minions
+            /* Currently acts weirdly, the dragon will remove a previous minion even when there is a slot available
+               this only happens if no dragon has been summoned yet. Probably has something to do with the for loop... */
+            if (proj.type != ModContent.ProjectileType<DragonMinionProj>() && proj.active && proj.owner == player.whoAmI)
+            {
+                if(player.numMinions < player.maxMinions + 1)
+                {
+                    proj.active = false;
+                    break;
+                }
             }
         }
-        
-        player.AddBuff(Item.buffType, 2);
 
-        var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
-        projectile.originalDamage = Item.damage;
+        player.AddBuff(Item.buffType, 2);
+        var projectile2 = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+        projectile2.originalDamage = Item.damage;
 
         return false;
+
     }
 
     // Needs a recipe or a source...
