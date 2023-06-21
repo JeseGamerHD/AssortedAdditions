df5252fe8d9bba4894bdeb1d2ab41f0e98abc791
JeseGamerHD <->
2023-06-21 19:20:52 +0300

    Added Fire Dragon Trophy

---
+++ Content/Items/Placeables/Trophies/FireDragonTrophy.cs
@@ -0,0 +1,27 @@
+﻿using ModdingTutorial.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Items.Placeables.Trophies
+{
+    internal class FireDragonTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.createTile = ModContent.TileType<BossTrophies>();
+            Item.placeStyle = 0;
+        }
+    }
+}
---
+++ Content/Tiles/BossTrophies.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Placeables.Trophies;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ModdingTutorial.Content.Tiles
+{
+    // This class is used for handling every boss trophy tile
+    internal class BossTrophies : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleWrapLimit = 36;
+            TileObjectData.addTile(Type);
+            DustType = 7;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Trophy");
+            AddMapEntry(new Color(120, 85, 60), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            int item = 0;
+            switch (frameX / 54)
+            {
+                case 0:
+                    item = ModContent.ItemType<FireDragonTrophy>();
+                    break;
+                // New trophies here
+            }
+            if (item > 0)
+            {
+                Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i, j), i * 16, j * 16, 48, 48, item);
+            }
+        }
+    }
+
+}
