ea0cd31b5397c0c4372aa0e03d7626d4aba5f14b
JeseGamerHD <->
2023-06-24 16:34:39 +0300

    Dune Ore now drops from some enemies

---
+++ Common/GlobalNPCs/ModifiedLootDrops.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+using ModdingTutorial.Content.Items.Placeables;
+using ModdingTutorial.Common.ItemDropRules;
+
+namespace ModdingTutorial.Common.GlobalNPCs
+{
+    // This class is for adding loot drops to existing NPCs/Monsters
+    internal class ModifiedLootDrops : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if(npc.type == NPCID.TombCrawlerHead)
+            {
+                // 33% chance to drop 3-7 dune ores after killing crimson/corruption boss
+                npcLoot.Add(new ItemDropWithConditionRule(ModContent.ItemType<DuneOre>(), 3, 4, 7, new WormOrBrainDefeated(), 1));
+            }
+
+            if (npc.type == NPCID.Antlion)
+            {
+                // ~15% chance to drop 2-4 dune ores after killing crimson/corruption boss
+                npcLoot.Add(new ItemDropWithConditionRule(ModContent.ItemType<DuneOre>(), 7, 2, 4, new WormOrBrainDefeated(), 1));
+            }
+        }
+    }
+}
---
+++ Common/ItemDropRules/WormOrBrainDefeated.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.GameContent.ItemDropRules;
+
+namespace ModdingTutorial.Common.ItemDropRules
+{
+    // Rule that checks if the 2nd boss has been defeated
+    public class WormOrBrainDefeated : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            return NPC.downedBoss2;
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "Drops after Eater of Worlds or Brain of Cthulhu has been defeated";
+        }
+    }
+}
