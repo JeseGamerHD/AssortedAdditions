08887f220a7b9171da1e14a5dc90574beedea004
JeseGamerHD <->
2023-06-30 18:39:06 +0300

    Added Dragon Scale

---
+++ Content/Items/Misc/DragonScale.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Misc
+{
+    internal class DragonScale : ModItem // Dropped from Fire Dragon boss
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 999;
+            Item.rare = ItemRarityID.LightRed;
+            Item.value = Item.sellPrice(silver: 40);
+        }
+    }
+}
