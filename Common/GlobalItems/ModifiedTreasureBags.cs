916d6daa236d7a2c3368f676c8d15a2426a3496b
JeseGamerHD <->
2023-07-01 16:19:32 +0300

    Ancient Token now obtainable from WoF

---
+++ Common/GlobalItems/ModifiedTreasureBags.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+using ModdingTutorial.Content.Items.Consumables;
+
+namespace ModdingTutorial.Common.GlobalItems
+{
+    // Class is used for modifying existing treasure bag loot
+    internal class ModifiedTreasureBags : GlobalItem
+    {
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            if(item.type == ItemID.WallOfFleshBossBag)
+            {
+                // This also drops if the player is on normal/journey, but directly from the boss (Added in GlobalNPCS -> ModifiedLootDrops.cs)
+                itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<AncientToken>(), 1, 1));
+            }
+        }
+    }
+}
--- Common/GlobalNPCs/ModifiedLootDrops.cs
+++ Common/GlobalNPCs/ModifiedLootDrops.cs
@@ -4,14 +4,26 @@ using Terraria.ModLoader;
 using Terraria.GameContent.ItemDropRules;
 using ModdingTutorial.Content.Items.Placeables;
 using ModdingTutorial.Common.ItemDropRules;
+using ModdingTutorial.Content.Items.Consumables;
 
 namespace ModdingTutorial.Common.GlobalNPCs
 {
-    // This class is for adding loot drops to existing NPCs/Monsters
+    // This class is for adding loot drops to existing NPCs
     internal class ModifiedLootDrops : GlobalNPC
     {
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
+            // BOSSES HERE:
+            if(npc.type == NPCID.WallofFlesh)
+            {
+                // Drops if playing on normal mode / Journey mode otherwise obtained from treasure bag
+                LeadingConditionRule notExpert = new LeadingConditionRule(new Conditions.NotExpert());
+                notExpert.OnSuccess(ItemDropRule.Common(ModContent.ItemType<AncientToken>(), 1, 1));
+                npcLoot.Add(notExpert);
+            }
+
+
+            // REGULAR ENEMIES UNDER HERE:
             if(npc.type == NPCID.TombCrawlerHead)
             {
                 // 33% chance to drop 4-7 dune ores aftercrimson/corruption boss has been defeated
@@ -23,6 +35,7 @@ namespace ModdingTutorial.Common.GlobalNPCs
                 // ~15% chance to drop 2-4 dune ores after crimson/corruption boss has been defeated
                 npcLoot.Add(new ItemDropWithConditionRule(ModContent.ItemType<DuneOre>(), 7, 2, 4, new WormOrBrainDefeated(), 1));
             }
+
         }
     }
 }
