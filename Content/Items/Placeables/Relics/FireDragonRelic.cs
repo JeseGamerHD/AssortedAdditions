53939870afee539965ff16d9538bd80451b0404c
JeseGamerHD-Laptop <->
2023-07-03 12:43:08 +0300

    Added Fire Dragon Relic

---
+++ Content/Items/Placeables/Relics/FireDragonRelic.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using ModdingTutorial.Content.Tiles.Relics;
+
+namespace ModdingTutorial.Content.Items.Placeables.Relics
+{
+    internal class FireDragonRelic : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 48;
+            Item.maxStack = 999;
+            Item.rare = ItemRarityID.Master;
+            Item.master = true; // This makes sure that "Master" displays in the tooltip, as the rarity only changes the item name color
+            Item.value = Item.sellPrice(gold: 5);
+
+            Item.DefaultToPlaceableTile(ModContent.TileType<FireDragonRelicTile>(), 0);
+        }
+    }
+}
