f8e8e91544c02204f9ab57a5429bb437ddfc6f0d
JeseGamerHD-Laptop <->
2023-07-03 17:21:57 +0300

    Added Dune Bow

---
+++ Content/Items/Weapons/Ranged/DuneBow.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using ModdingTutorial.Content.Items.Placeables;
+
+namespace ModdingTutorial.Content.Items.Weapons.Ranged
+{
+    internal class DuneBow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 36;
+
+            Item.damage = 30;
+            Item.shootSpeed = 8;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            
+            Item.noMelee = true;
+            Item.autoReuse = true;
+
+            Item.DamageType = DamageClass.Ranged;
+            Item.rare = ItemRarityID.Orange;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.Item5;
+            Item.value = Item.sellPrice(silver: 60);
+
+            Item.useAmmo = AmmoID.Arrow; // Also determines which arrow projectile is fired
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<DuneBar>(), 9);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
