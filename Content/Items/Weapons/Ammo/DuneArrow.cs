afb579a656b9454cf73380e32c248d212e0f5b66
JeseGamerHD-Laptop <->
2023-07-03 18:14:49 +0300

    Added Dune Arrows

---
+++ Content/Items/Weapons/Ammo/DuneArrow.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using ModdingTutorial.Content.Projectiles.RangedProj;
+using ModdingTutorial.Content.Items.Placeables;
+
+namespace ModdingTutorial.Content.Items.Weapons.Ammo
+{
+    internal class DuneArrow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 34;
+            Item.damage = 12;
+            Item.maxStack = 999;
+            Item.shootSpeed = 6.5f;
+
+            Item.consumable = true;
+
+            Item.value = Item.sellPrice(copper: 20);
+            Item.DamageType = DamageClass.Ranged;
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<DuneArrowProj>();
+            Item.ammo = AmmoID.Arrow;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe(25); // Makes 25 per craft
+            recipe.AddIngredient(ModContent.ItemType<DuneOre>(), 1);
+            recipe.AddRecipeGroup("Wood");
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Projectiles/RangedProj/DuneArrowProj.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Projectiles.RangedProj
+{
+    internal class DuneArrowProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 18;
+            Projectile.height = 34;
+            Projectile.damage = 0;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+
+            Projectile.aiStyle = 0;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() +  MathHelper.PiOver2;
+
+            // Basic arrow gravity
+            Projectile.velocity.Y = Projectile.velocity.Y + 0.05f;
+            if (Projectile.velocity.Y > 16f)
+            {
+                Projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height); // Dust from tile when hit
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.Center); // Tile hit sound
+            return true;
+        }
+    }
+}
