6239e38b8295b9e16e95cdb7bc1af14ae35f0790
JeseGamerHD-Laptop <->
2023-07-13 18:42:24 +0300

    Added Bo Staffs

---
+++ Content/Items/Weapons/Melee/BoStaffs/BoStaff.cs
@@ -0,0 +1,139 @@
+﻿using ModdingTutorial.Content.Projectiles.MeleeProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Weapons.Melee.BoStaffs
+{
+    // Other types of BoStaff inherit this class, only changing the recipe depending on the wood type
+    // Projectile visuals change inside BoStaffSpin depending on which staff is being used
+    internal class BoStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 82;
+            Item.height = 82;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.damage = 4;
+            Item.knockBack = 4;
+            Item.shootSpeed = 5f;
+
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.channel = true;
+            Item.autoReuse = true;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.rare = ItemRarityID.White;
+            Item.shoot = ModContent.ProjectileType<BoStaffSpin>();
+            Item.value = Item.sellPrice(copper: 20);
+            Item.DamageType = DamageClass.Melee;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[Item.shoot] < 1;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Wood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+
+    // All the other variants are under here:
+    internal class MahoganyBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.RichMahogany, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class AshBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.AshWood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class BorealBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.BorealWood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class DynastyBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DynastyWood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class EbonWoodBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Ebonwood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class PalmBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.PalmWood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class PearlWoodBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Pearlwood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class ShadeWoodBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Shadewood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+    internal class SpookyBoStaff : BoStaff
+    {
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.SpookyWood, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Projectiles/MeleeProj/BoStaffSpin.cs
@@ -0,0 +1,153 @@
+﻿using Terraria.Audio;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Weapons.Melee.BoStaffs;
+
+namespace ModdingTutorial.Content.Projectiles.MeleeProj
+{
+    internal class BoStaffSpin : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 10;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 82;
+            Projectile.height = 82;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            // Check if Player Dead
+            if (player.dead)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            // Weapon will spin as long as player keeps channeling
+            if (player.channel)
+            {
+                Projectile.position = player.Center; // Needs to be adjusted below
+                Projectile.position.Y -= 40;
+
+                if (player.direction == -1) // If player faces left adjust accordingly
+                {
+                    Projectile.position.X -= 40;
+                    Projectile.rotation -= 0.2f; // Rotation also changes depending on direction faced
+                }
+                else // If right, adjust differently
+                {
+                    Projectile.position.X -= 45;
+                    Projectile.rotation += 0.2f;
+                }
+
+                // Used for swapping the weapon position if the player switches direction
+                int switchSides = player.direction;
+
+                // Weapon needs to "point" towards the cursor's X position
+                if (Main.MouseWorld.X > player.Center.X)
+                {
+                    player.ChangeDir(1); // Change player to face said direction
+                }
+                else if (Main.MouseWorld.X < player.Center.X)
+                {
+                    player.ChangeDir(-1);
+                }
+
+                // Replaces the old projectile with a new one after switching direction
+                // New projectile alligns properly
+                if (switchSides != player.direction)
+                {
+                    Projectile.Kill();
+                }
+
+                // Makes player hold the weapon
+                player.heldProj = Projectile.whoAmI;
+                player.itemTime = 2;
+                player.itemAnimation = 2;
+                player.itemRotation = MathHelper.WrapAngle(Projectile.rotation);
+
+                // Sprite changes depending on which staff is being used
+                Visuals();
+
+                // Sound effect
+                if (Projectile.soundDelay == 0)
+                {
+                    Projectile.soundDelay = 14; // This countsdown automatically
+                    SoundEngine.PlaySound(SoundID.Item1, Projectile.position);
+                }
+
+            }
+            else // When channeling stops, the projectile is destroyed
+            {
+                Projectile.Kill();
+            }
+        }
+
+        private void Visuals()
+        {
+            Player player = Main.player[Projectile.owner];
+            int itemInUse = player.inventory[player.selectedItem].type; // The item the player is currently using
+
+            if (itemInUse == ModContent.ItemType<AshBoStaff>())
+            {
+                Projectile.frame = 0;
+            }
+
+            if (itemInUse == ModContent.ItemType<BorealBoStaff>())
+            {
+                Projectile.frame = 1;
+            }
+
+            if (itemInUse == ModContent.ItemType<BoStaff>())
+            {
+                Projectile.frame = 2;
+            }
+
+            if (itemInUse == ModContent.ItemType<DynastyBoStaff>())
+            {
+                Projectile.frame = 3;
+            }
+
+            if (itemInUse == ModContent.ItemType<EbonWoodBoStaff>())
+            {
+                Projectile.frame = 4;
+            }
+
+            if (itemInUse == ModContent.ItemType<MahoganyBoStaff>())
+            {
+                Projectile.frame = 5;
+            }
+
+            if (itemInUse == ModContent.ItemType<PalmBoStaff>())
+            {
+                Projectile.frame = 6;
+            }
+
+            if (itemInUse == ModContent.ItemType<PearlWoodBoStaff>())
+            {
+                Projectile.frame = 7;
+            }
+
+            if (itemInUse == ModContent.ItemType<ShadeWoodBoStaff>())
+            {
+                Projectile.frame = 8;
+            }
+
+            if (itemInUse == ModContent.ItemType<SpookyBoStaff>())
+            {
+                Projectile.frame = 9;
+            }
+        }
+    }
+}
