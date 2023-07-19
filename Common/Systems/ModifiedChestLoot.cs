c4c26b262b226579bb7c73f84eea5deffb58e14c
JeseGamerHD <->
2023-07-19 15:02:41 +0300

    Added Miner's Ring

--- Common/Systems/ModifiedChestLoot.cs
+++ Common/Systems/ModifiedChestLoot.cs
@@ -2,27 +2,59 @@
 using Terraria.ID;
 using Terraria;
 using ModdingTutorial.Content.Items.Weapons.Ranged;
+using ModdingTutorial.Content.Items.Accessories;
 
 namespace ModdingTutorial.Common.Systems
 {
+    // This class is used for adding items to existing chests' loot pools
     internal class ModifiedChestLoot : ModSystem
-    { // This class is used for adding items to existing chest loot pools
+    {
 
         public override void PostWorldGen()
         {
+
+            int chestWidth = 36; // 36 is the width of the chest tiles
+     
+            // Chests can be found within tiles_21 (TileID.Containers) and tiles_467 (TileID.Containers2) spritesheets
+            // Below are ID's for different chests, counting begins from zero:
+            
+            // Containers
+            int ChestID = 0;
+
+            // Containers2
+            int SandstoneChestID = 10;
+            
+
             for (int chestIndex = 0; chestIndex < 8000; chestIndex++)
             {
                 Chest chest = Main.chest[chestIndex];
-                int chestID = 10; //Sandstone Chest, actual SubID is one lower since counting begins from 0
-                int chestWidth = 36; // 36 is the width of the tile
 
-                if (chest != null && Main.tile[chest.x, chest.y].TileType == TileID.Containers2 && Main.tile[chest.x, chest.y].TileFrameX == chestID * chestWidth)
+                // Regular chests
+                if (chest != null && Main.tile[chest.x, chest.y].TileType == TileID.Containers && Main.tile[chest.x, chest.y].TileFrameX == ChestID * chestWidth)
                 {
                     for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++)
                     {
                         if (chest.item[inventoryIndex].type == ItemID.None)
                         {
-                            // 33% chance to be in the chest
+                            // 20% chance to be in a chest
+                            if (Main.rand.NextFloat() <= 0.2f)
+                            {
+                                chest.item[inventoryIndex].SetDefaults(ModContent.ItemType<MinersRing>());
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                // Sandstone chests
+                if (chest != null && Main.tile[chest.x, chest.y].TileType == TileID.Containers2 && Main.tile[chest.x, chest.y].TileFrameX == SandstoneChestID * chestWidth)
+                {
+                    for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++)
+                    {
+                        if (chest.item[inventoryIndex].type == ItemID.None)
+                        {
+                            // 33% chance to be in a chest
                             if(Main.rand.NextFloat() <= 0.3333f)
                             {
                                 chest.item[inventoryIndex].SetDefaults(ModContent.ItemType<Dunerang>());
@@ -32,9 +64,9 @@ namespace ModdingTutorial.Common.Systems
                         }
                     }
                 }
-            }
 
-            // Additional chests here
+                // ...
+            }
         }
 
     }
---
+++ Content/Items/Accessories/MinersRing.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace ModdingTutorial.Content.Items.Accessories
+{
+    internal class MinersRing : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(gold: 2);
+            Item.rare = ItemRarityID.Blue;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.pickSpeed += 0.25f; // Increases mining speed by 25%
+        }
+    }
+}
