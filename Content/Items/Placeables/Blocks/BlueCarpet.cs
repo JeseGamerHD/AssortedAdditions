c7bdf081bd9a456240930932ad56eb01b46790fa
JeseGamerHD <->
2023-07-24 13:28:18 +0300

    More carpets...

---
+++ Content/Items/Placeables/Blocks/BlueCarpet.cs
@@ -0,0 +1,27 @@
+﻿using ModdingTutorial.Content.Tiles.Blocks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Placeables.Blocks
+{
+    internal class BlueCarpet : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 999;
+            Item.consumable = true;
+            Item.value = Item.sellPrice(copper: 75);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+
+            Item.createTile = ModContent.TileType<BlueCarpetTile>();
+        }
+    }
+}
---
+++ Content/Tiles/Blocks/GreenCarpetTile.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Tiles.Blocks
+{
+    internal class GreenCarpetTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
+
+            AddMapEntry(new Color(26, 188, 74));
+            DustType = DustID.GemEmerald;
+            HitSound = SoundID.Dig;
+        }
+    }
+}
