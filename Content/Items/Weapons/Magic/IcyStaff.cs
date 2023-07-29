4c62c1afaa4a2c1780dcae01b4004a572fdcfa39
JeseGamerHD <->
2023-07-29 15:19:12 +0300

    Added Icy Wand (needs recipe)

---
+++ Content/Items/Weapons/Magic/IcyStaff.cs
@@ -0,0 +1,44 @@
+﻿using ModdingTutorial.Content.Projectiles.MagicProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Items.Weapons.Magic
+{
+    internal class IcyStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 69;
+            Item.height = 68;
+            Item.damage = 48;
+            Item.knockBack = 0;
+            Item.useAnimation = 10;
+            Item.useTime = 10;
+            Item.shootSpeed = 14;
+            Item.mana = 5;
+            Item.scale = 0.9f;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.DamageType = DamageClass.Magic;
+            Item.rare = ItemRarityID.Pink;
+            Item.value = Item.sellPrice(gold: 4);
+            Item.UseSound = SoundID.Item9;
+            Item.shoot = ModContent.ProjectileType<IcyWandProj>();
+
+            Item.autoReuse = true;
+            Item.noMelee = true;
+        }
+
+        public override Vector2? HoldoutOffset() => new(-15, 0); // Used for alligning the sprite
+
+        // Makes the projectiles have a spread
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            velocity = velocity.RotatedByRandom(MathHelper.ToRadians(8));
+        }
+
+        // TODO Source/Recipe?
+    }
+}
---
+++ Content/Projectiles/MagicProj/IcyWandProj.cs
@@ -0,0 +1,71 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Projectiles.MagicProj
+{
+    internal class IcyWandProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.alpha = 0;
+            Projectile.penetrate = -1;
+            Projectile.aiStyle = 0;
+            Projectile.scale = 0.5f;
+
+            Projectile.DamageType = DamageClass.Magic;
+
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            Projectile.hostile = false;
+            Projectile.friendly = true;
+        }
+
+        private bool setOnce = false;
+        public override void AI()
+        {
+            // Set the frame to 1 of 3 possible ones to create some variation in the projectiles
+            // Also randomize rotation
+            if (!setOnce)
+            {
+                int frame = Main.rand.Next(0, 3);
+                switch (frame)
+                {
+                    case 0:
+                        Projectile.frame = 0;
+                        Projectile.rotation = frame;
+                        break;
+                    case 1:
+                        Projectile.frame = 1;
+                        Projectile.rotation = frame;
+                        break;
+                    case 2:
+                        Projectile.frame = 2;
+                        Projectile.rotation = frame;
+                        break;
+                }
+                setOnce = true;
+            }
+
+            // Dust stuff
+            if (Main.rand.NextBool(3))
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position - Projectile.velocity, Projectile.width, Projectile.height,
+                        DustID.SnowSpray, 0, 0, 100, default, 0.5f);
+                dust.noGravity = false;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Frostburn, 180);
+        }
+    }
+}
