e7d3b00a66aaadcfd7aa6fcb46b706cf9ff19b9a
JeseGamerHD <->
2023-07-30 16:17:23 +0300

    Added Frost Bar

---
+++ Content/Items/Placeables/Ores/FrostBar.cs
@@ -0,0 +1,42 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using ModdingTutorial.Content.Tiles;
+
+namespace ModdingTutorial.Content.Items.Placeables.Ores
+{
+    internal class FrostBar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 58;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 999;
+            Item.consumable = true;
+            Item.value = Item.sellPrice(silver: 60);
+            Item.rare = ItemRarityID.Green;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+
+            Item.createTile = ModContent.TileType<FrostBarTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<Permafrost>(), 4);
+            recipe.AddTile(TileID.IceMachine);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/FrostBarTile.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ObjectData;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Tiles
+{
+    internal class FrostBarTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileShine[Type] = 1100;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(200, 200, 200));
+        }
+    }
+}
