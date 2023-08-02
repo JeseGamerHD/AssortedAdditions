2b0cd342a462ff94b3f0d5ddc099b9fe548e3564
JeseGamerHD <->
2023-08-02 14:08:32 +0300

    Added Aurora Blade

---
+++ Content/Items/Weapons/Melee/AuroraBlade.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Misc;
+using ModdingTutorial.Content.Items.Placeables.Ores;
+using ModdingTutorial.Content.Projectiles.MeleeProj;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Items.Weapons.Melee;
+
+// This is a copy of the Excalibur (from ExampleMod)
+public class AuroraBlade : ModItem
+{
+    public override void SetDefaults()
+    {
+        
+        Item.useAnimation = 20;
+        Item.useTime = 20;
+        Item.damage = 70;
+        Item.knockBack = 4.5f;
+        Item.width = 72;
+        Item.height = 72;
+        Item.scale = 1f;
+
+        Item.noMelee = true; // This is set the sword itself doesn't deal damage (only the projectile does).
+        Item.shootsEveryUse = true; // This makes sure Player.ItemAnimationJustStarted is set when swinging.
+        Item.autoReuse = true;
+
+        Item.value = Item.sellPrice(gold: 8);
+        Item.UseSound = SoundID.Item1;
+        Item.rare = ItemRarityID.Pink;
+        Item.DamageType = DamageClass.Melee;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.shoot = ModContent.ProjectileType<AuroraBladeProj>();
+
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        float adjustedItemScale = player.GetAdjustedItemScale(Item); // Get the melee scale of the player and item.
+        Projectile.NewProjectile(source, player.MountedCenter, new Vector2(player.direction, 0f), type, damage, knockback, player.whoAmI, player.direction * player.gravDir, player.itemAnimationMax, adjustedItemScale);
+        NetMessage.SendData(MessageID.PlayerControls, -1, -1, null, player.whoAmI); // Sync the changes in multiplayer.
+
+        return base.Shoot(player, source, position, velocity, type, damage, knockback);
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 15);
+        recipe.AddIngredient(ModContent.ItemType<IceEssence>(), 8);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+    }
+}
