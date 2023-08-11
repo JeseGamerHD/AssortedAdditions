364b9fe9e0b416e5d2f3cc602d6d3882c2429ace
JeseGamerHD <->
2023-08-11 11:47:57 +0300

    Sharpening Station, Crystal Ball, Ammo Box and Bewitching Table now give their buff when the item is in any personal inventory.

---
+++ Common/GlobalItems/ModifiedActivatedFurniture.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Common.GlobalItems
+{
+    // This class is used for modifying furniture items that grant a buff
+    // E.g Sharpening Station, Ammo Box...
+    // These items can still be used normally as well
+    internal class ModifiedActivatedFurniture : GlobalItem
+    {
+        // Make these items give the buff when the item is in player's inventory
+        public override void UpdateInventory(Item item, Player player)
+        {
+            if(item.type == ItemID.AmmoBox)
+            {
+                player.AddBuff(BuffID.AmmoBox, 2);
+            }
+
+            if(item.type == ItemID.CrystalBall)
+            {
+                player.AddBuff(BuffID.Clairvoyance, 2);
+            }
+
+            if(item.type == ItemID.SharpeningStation)
+            {
+                player.AddBuff(BuffID.Sharpened, 2);
+            }
+
+            if (item.type == ItemID.BewitchingTable)
+            {
+                player.AddBuff(BuffID.Bewitched, 2);
+            }
+        }
+
+        // Add a tooltip to tell the player about this change
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.type == ItemID.AmmoBox)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", "[c/FFF014:Gives the Ammo Box buff while in any personal inventory]"));
+            }
+
+            if (item.type == ItemID.CrystalBall)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", "[c/FFF014:Gives the Clairvoyance buff while in any personal inventory]"));
+            }
+
+            if (item.type == ItemID.SharpeningStation)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", "[c/FFF014:Gives the Sharpened buff while in any personal inventory]"));
+            }
+
+            if (item.type == ItemID.BewitchingTable)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", "[c/FFF014:Gives the Bewitched buff while in any personal inventory]"));
+            }
+        }
+    }
+}
---
+++ Common/Players/SaveInventoryBuffs.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Common.Players
+{
+    // Used for giving player the buff from items that give a buff inside the inventory
+    // when loading back into a world
+    internal class SaveInventoryBuffs : ModPlayer
+    {
+        public override void PreUpdateBuffs()
+        {
+            if (Player.HasItemInAnyInventory(ItemID.SharpeningStation))
+            {
+                Player.AddBuff(BuffID.Sharpened, 2);
+            }
+
+            if (Player.HasItemInAnyInventory(ItemID.BewitchingTable))
+            {
+                Player.AddBuff(BuffID.Bewitched, 2);
+            }
+
+            if (Player.HasItemInAnyInventory(ItemID.AmmoBox))
+            {
+                Player.AddBuff(BuffID.AmmoBox, 2);
+            }
+
+            if (Player.HasItemInAnyInventory(ItemID.CrystalBall))
+            {
+                Player.AddBuff(BuffID.Clairvoyance, 2);
+            }
+        }
+    }
+}
