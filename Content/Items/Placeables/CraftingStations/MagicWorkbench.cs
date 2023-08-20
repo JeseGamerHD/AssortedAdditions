86aae184f3c8322c7edd37e6d6f9933d2f65ad85
JeseGamerHD <->
2023-08-20 12:59:25 +0300

    Added Magic Work Bench

---
+++ Content/Items/Placeables/CraftingStations/MagicWorkbench.cs
@@ -0,0 +1,39 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using ModdingTutorial.Content.Tiles.CraftingStations;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Placeables.CraftingStations
+{
+    internal class MagicWorkbench : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 23;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.maxStack = 9999;
+
+            Item.consumable = true;
+            Item.autoReuse = true;
+            Item.useTurn = true;
+
+            Item.value = Item.sellPrice(gold: 3);
+            Item.rare = ItemRarityID.Orange;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.createTile = ModContent.TileType<MagicWorkbenchTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CrystalBall, 1);
+            recipe.AddIngredient(ItemID.WorkBench, 1);
+            recipe.AddIngredient(ItemID.Book, 15);
+            recipe.AddIngredient(ItemID.StoneBlock, 25);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/CraftingStations/MagicWorkbenchTile.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ModdingTutorial.Content.Tiles.CraftingStations
+{
+    internal class MagicWorkbenchTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+
+            DustType = DustID.TintablePaint;
+            HitSound = SoundID.Dig;
+
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+            TileObjectData.addTile(Type);
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            AddMapEntry(new Microsoft.Xna.Framework.Color(119, 44, 120), CreateMapEntryName());
+        }
+    }
+}
