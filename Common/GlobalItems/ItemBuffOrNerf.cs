45835ce8bc0187bcfa9904f93b34c8b7b5975cdf
JeseGamerHD <->
2023-08-23 09:12:53 +0300

    Buffed Golden Fishing Rod (from 50% => 75% fishing power)

---
+++ Common/GlobalItems/ItemBuffOrNerf.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Common.GlobalItems
+{
+    // This class is used for either buffing some item or nerfing it 
+    internal class ItemBuffOrNerf : GlobalItem
+    {
+        public override void SetDefaults(Item entity)
+        {
+            // Make the golden fishing rod a bit better
+            if(entity.type == ItemID.GoldenFishingRod)
+            {
+                entity.fishingPole = 75; // from 50% => 75%
+            }
+        }
+    }
+}
