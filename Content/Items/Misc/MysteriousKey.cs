0f5ea5a1213f369f418214522e24b1a12afc038c
JeseGamerHD <->
2023-08-26 10:17:27 +0300

    Added Mysterious Chest + Mysterious Key

---
+++ Content/Items/Misc/MysteriousKey.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Tiles.CraftingStations;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Items.Misc
+{
+    internal class MysteriousKey : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(8, 4));
+            ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.GoldenKey);
+            Item.rare = ItemRarityID.LightPurple;
+            Item.UseSound = SoundID.Item8;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if(Main.myPlayer == player.whoAmI)
+            {
+                // Remember to multiply by 16
+                Vector2 roomPos = new((Main.dungeonX + 3) * 16, (Main.dungeonY + 16) * 16);
+                player.Teleport(roomPos, TeleportationStyleID.TeleportationPotion);
+            }
+
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.SoulofMight, 1);
+            recipe.AddIngredient(ItemID.SoulofSight, 1);
+            recipe.AddIngredient(ItemID.SoulofFright, 1);
+            recipe.AddIngredient(ItemID.ShadowKey, 1);
+            recipe.AddTile(ModContent.TileType<MagicWorkbenchTile>());
+            recipe.Register();
+        }
+
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(Item.Center, Color.Purple.ToVector3() * 0.55f * Main.essScale); // Makes this item glow when thrown out of inventory.
+        }
+    }
+}
---
+++ Content/Items/Placeables/Furniture/MysteriousChest.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using ModdingTutorial.Content.Tiles.Furniture;
+using Terraria.ID;
+using ModdingTutorial.Content.Items.Placeables.Blocks;
+
+namespace ModdingTutorial.Content.Items.Placeables.Furniture
+{
+    internal class MysteriousChest : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<MysteriousChestTile>());
+            
+            Item.width = 32;
+            Item.height = 28;
+            Item.value = Item.sellPrice(silver: 10);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<MysteriousBrick>(), 5);
+            recipe.AddIngredient(ItemID.GraniteBlock, 5);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
