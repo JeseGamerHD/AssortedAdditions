7c9621b2e1cbe7b9413686c7f96ad5aae3ff68a6
JeseGamerHD <->
2023-08-28 16:32:30 +0300

    Added Shooting Star (ranged weapon)

---
+++ Content/Items/Weapons/Ranged/ShootingStar.cs
@@ -0,0 +1,41 @@
+﻿using ModdingTutorial.Content.Projectiles.RangedProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Items.Weapons.Ranged
+{
+    internal class ShootingStar : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 36;
+            Item.height = 76;
+            Item.damage = 70;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.knockBack = 6f;
+            Item.shootSpeed = 12;
+
+            Item.noMelee = true;
+            Item.autoReuse = true;
+
+            Item.rare = ItemRarityID.Lime;
+            Item.DamageType = DamageClass.Ranged;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.Item9;
+            Item.value = Item.sellPrice(gold: 10);
+            Item.useAmmo = AmmoID.Arrow;
+            Item.shoot = ModContent.ProjectileType<ShootingStarProj>();
+        }
+
+        public override Vector2? HoldoutOffset() => new(-10, 2); // Alligns the sprite properly
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            // Always shoots custom projectile no matter which arrow was used
+            type = ModContent.ProjectileType<ShootingStarProj>();
+        }
+    }
+}
---
+++ Content/Projectiles/RangedProj/ShootingStarProj.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Projectiles.RangedProj
+{
+    internal class ShootingStarProj : ModProjectile
+    {
+        public override void SetStaticDefaults() // These are needed for shader trail
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 45;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 22;
+            Projectile.height = 24;
+            Projectile.light = 1f;
+            Projectile.alpha = 50;
+            Projectile.aiStyle = 0;
+            Projectile.timeLeft = 300;
+
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.friendly = true;
+
+            Projectile.DamageType = DamageClass.Ranged;
+        }
+
+        public float Timer
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override void AI()
+        {
+            Timer++;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            if(Timer > 30)
+            {
+                Projectile.velocity.Y = Projectile.velocity.Y + 0.3f; // 0.1f for arrow gravity, 0.4f for knife gravity
+                if (Projectile.velocity.Y > 16f) // This check implements "terminal velocity". We don't want the projectile to keep getting faster and faster. Past 16f this projectile will travel through blocks, so this check is useful.
+                {
+                    Projectile.velocity.Y = 16f;
+                }
+            }
+
+            // Leave a dust trail
+            if (Main.rand.NextBool(2))
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position - Projectile.velocity, Projectile.width, Projectile.height,
+                            DustID.CorruptTorch, 0, 0, 150, default, 1f);
+                dust.noGravity = true;
+
+                Dust dust2 = Dust.NewDustDirect(Projectile.position - Projectile.velocity, Projectile.width, Projectile.height,
+                            DustID.PlatinumCoin, 0, 0, 150, default, 2f);
+                dust2.noGravity = true;
+            }
+
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.PurpleTorch, 0f, 0f, 100, default, 2f);
+                dust.velocity *= 1.4f;
+                dust.noGravity = true;
+            }
+        }
+
+        // The cool trail effect happens under here:
+
+        private static readonly VertexStrip _vertexStrip = new();
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            MiscShaderData miscShaderData = GameShaders.Misc["MagicMissile"]; // Basically a copy of magic missile with its colors changed
+            miscShaderData.UseSaturation(-2.8f);
+            miscShaderData.UseOpacity(4f);
+            miscShaderData.Apply();
+            _vertexStrip.PrepareStripWithProceduralPadding(Projectile.oldPos, Projectile.oldRot, StripColors, StripWidth, -Main.screenPosition + Projectile.Size / 2);
+            _vertexStrip.DrawTrail();
+            Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+
+            return true;
+        }
+
+        private Color StripColors(float progressOnStrip)
+        {
+            Color result = Color.Lerp(Color.Indigo, Color.Indigo, Utils.GetLerpValue(-0.2f, 0.5f, progressOnStrip, clamped: true)) * (1f - Utils.GetLerpValue(0f, 0.98f, progressOnStrip));
+            result.A = 0;
+            return result;
+        }
+
+        private float StripWidth(float progressOnStrip)
+        {
+            float num = 1f;
+            float lerpValue = Utils.GetLerpValue(0f, 0.2f, progressOnStrip, clamped: true);
+            num *= 1f - (1f - lerpValue) * (1f - lerpValue);
+            return MathHelper.Lerp(0f, 32f, num);
+        }
+    }
+}
