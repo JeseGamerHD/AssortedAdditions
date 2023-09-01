2895bf8af3bab81d8a6c2448ab40e475d49ab224
JeseGamerHD <->
2023-09-01 16:15:15 +0300

    Added new summon weapon

---
+++ Content/Items/Weapons/Summon/SnowDwellerStaff.cs
@@ -0,0 +1,66 @@
+﻿using ModdingTutorial.Content.Buffs;
+using ModdingTutorial.Content.Projectiles.SummonProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Misc;
+using ModdingTutorial.Content.Items.Placeables.Ores;
+
+namespace ModdingTutorial.Content.Items.Weapons.Summon
+{
+    internal class SnowDwellerStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.mana = 20;
+            Item.damage = 50;
+            Item.knockBack = 2f;
+            Item.scale = 2f;
+
+            Item.noMelee = true;
+
+            Item.rare = ItemRarityID.Pink;
+            Item.DamageType = DamageClass.Summon;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item44;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.buffType = ModContent.BuffType<SnowDwellerBuff>();
+            Item.shoot = ModContent.ProjectileType<SnowDwellerMinion>();
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = Main.MouseWorld; // Spawns at cursor position
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            // This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+            player.AddBuff(Item.buffType, 2);
+
+            // Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+
+            // Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.SnowBlock, 20);
+            recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 16);
+            recipe.AddIngredient(ModContent.ItemType<IceEssence>(), 6);
+            recipe.AddTile(TileID.IceMachine);
+            recipe.Register();
+        }
+
+    }
+}
---
+++ Content/Projectiles/SummonProj/SnowDwellerMinion.cs
@@ -0,0 +1,66 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using ModdingTutorial.Content.Buffs;
+
+namespace ModdingTutorial.Content.Projectiles.SummonProj
+{
+    internal class SnowDwellerMinion : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projPet[Projectile.type] = true; // Denotes that this projectile is a pet or minion
+            Main.projFrames[Projectile.type] = 15;
+
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true; // This is needed so your minion can properly spawn when summoned and replaced when other minions are summoned
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true; // Make the cultist resistant to this projectile, as it's resistant to all homing projectiles.
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 36;
+            Projectile.height = 40;
+            Projectile.penetrate = -1;
+            Projectile.minionSlots = 1f;
+
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+            Projectile.minion = true;
+
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.aiStyle = 67; // Pirate staff
+            AIType = ProjectileID.OneEyedPirate; 
+            // The downside to using this is that the minion poops
+            // although it sort of fits the creature...
+        }
+
+        // Despawn minion if buff is removed
+        // Otherwise keep minion alive
+        public override bool PreAI()
+        {
+            Player owner = Main.player[Projectile.owner];
+
+            if (owner.HasBuff(ModContent.BuffType<SnowDwellerBuff>()))
+            {
+                Projectile.timeLeft = 2;
+                return true;
+            }
+            else
+            {
+                Projectile.Kill();
+                return false;
+            }
+        }
+
+        public override bool? CanCutTiles()
+        {
+            return false;
+        }
+
+        // Minion won't deal damage without this (unless it fires projectiles)
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+    }
+}
