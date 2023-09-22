19d6a98b5bf8934c9064dab256c00c49e31ccc3a
JeseGamerHD <->
2023-09-22 15:17:31 +0300

    Added Antidote

---
+++ Content/Items/Accessories/Antidote.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Accessories
+{
+    internal class Antidote : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(gold: 2);
+            Item.rare = ItemRarityID.LightRed;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.buffImmune[BuffID.Rabies] = true;
+        }
+    }
+}
