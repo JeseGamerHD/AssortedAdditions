823685ffd9f6977a9bbe1556cef16964bbc16f38
JeseGamerHD <->
2023-09-23 15:50:07 +0300

    Added Fiber Glass

---
+++ Content/Items/Placeables/Blocks/FiberGlass.cs
@@ -0,0 +1,36 @@
+﻿using ModdingTutorial.Content.Tiles.Blocks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ModdingTutorial.Content.Items.Placeables.Blocks
+{
+    internal class FiberGlass : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 999;
+            Item.consumable = true;
+            Item.value = Item.sellPrice(copper: 60);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+
+            Item.createTile = ModContent.TileType<FiberGlassTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe(3);
+            recipe.AddIngredient(ItemID.Glass, 3);
+            recipe.AddIngredient(ItemID.Gel, 1);
+            recipe.AddTile(TileID.GlassKiln);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/Blocks/FiberGlassTile.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Tiles.Blocks
+{
+    internal class FiberGlassTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = false;
+
+            AddMapEntry(new Color(118, 200, 232));
+            DustType = 13;
+            HitSound = SoundID.Shatter;
+        }
+    }
+}
