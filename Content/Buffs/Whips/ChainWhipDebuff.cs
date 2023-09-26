1dbe73e2b2609ed1ed8f772e94e3ee17ad58ab81
JeseGamerHD <->
2023-09-26 18:08:38 +0300

    Added Chain Whip

---
+++ Content/Buffs/Whips/ChainWhipDebuff.cs
@@ -0,0 +1,39 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Buffs.Whips
+{
+    internal class ChainWhipDebuff : ModBuff
+    {
+        public override string Texture => "ModdingTutorial/Content/Buffs/Whips/MotivatorDebuff";
+
+        public static readonly int TagDamage = 6;
+
+        public override void SetStaticDefaults()
+        {
+            // This allows the debuff to be inflicted on NPCs that would otherwise be immune to all debuffs.
+            // Other mods may check it for different purposes.
+            BuffID.Sets.IsAnNPCWhipDebuff[Type] = true;
+            Main.debuff[Type] = true;
+        }
+
+        public class CHainWhipDebuffNPC : GlobalNPC
+        {
+            public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref NPC.HitModifiers modifiers)
+            {
+                // Only player attacks should benefit from this buff, hence the NPC and trap checks.
+                if (projectile.npcProj || projectile.trap || !projectile.IsMinionOrSentryRelated)
+                    return;
+
+                // SummonTagDamageMultiplier scales down tag damage for some specific minion and sentry projectiles for balance purposes.
+                var projTagMultiplier = ProjectileID.Sets.SummonTagDamageMultiplier[projectile.type];
+                if (npc.HasBuff<CosmicWhipDebuff>())
+                {
+                    // Apply a flat bonus to every hit
+                    modifiers.FlatBonusDamage += MotivatorDebuff.TagDamage * projTagMultiplier;
+                }
+            }
+        }
+    }
+}
---
+++ Content/Items/Weapons/Summon/ChainWhip.cs
@@ -0,0 +1,34 @@
+﻿using ModdingTutorial.Content.Projectiles.SummonProj;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Items.Weapons.Summon
+{
+    internal class ChainWhip : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(ModContent.ProjectileType<ChainWhipProj>(), 18, 2, 8);
+            Item.value = Item.sellPrice(silver: 20);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        // Makes the whip receive melee prefixes
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddRecipeGroup("IronBar", 5);
+            recipe.AddIngredient(ItemID.Chain, 30);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
