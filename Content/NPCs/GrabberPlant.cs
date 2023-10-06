449083901773a6aa4886944e8d62467277b0c0df
JeseGamerHD <->
2023-10-06 13:48:51 +0300

    Added Grabber Plant NPC + its banner

---
+++ Content/NPCs/GrabberPlant.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Placeables.Banners;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace ModdingTutorial.Content.NPCs
+{
+    internal class GrabberPlant : ModNPC // TODO
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 8;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 50;
+            NPC.height = 40;
+            NPC.damage = 15;
+            NPC.defense = 6;
+            NPC.lifeMax = 100;
+            NPC.knockBackResist = 0;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.value = 200;
+
+            NPC.aiStyle = 0;
+
+            Banner = NPC.type;
+            BannerItem = ModContent.ItemType<GrabberPlantBanner>();
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            // Surface
+            if (spawnInfo.Player.ZoneJungle && !spawnInfo.Player.ZoneRockLayerHeight)
+            {
+                if(!Main.hardMode)
+                {
+                    return SpawnCondition.SurfaceJungle.Chance;
+                }
+                else
+                {
+                    return SpawnCondition.SurfaceJungle.Chance * 0.5f;
+                }
+            }
+            // Underground
+            else if(spawnInfo.Player.ZoneJungle && spawnInfo.Player.ZoneRockLayerHeight)
+            {
+                if(!Main.hardMode)
+                {
+                    return SpawnCondition.UndergroundJungle.Chance;
+                }
+                else
+                {
+                    return SpawnCondition.UndergroundJungle.Chance * 0.5f;
+                }
+            }
+            
+            return 0f;
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            // TODO
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            // Create gore when the NPC is killed.
+            if (Main.netMode != NetmodeID.Server && NPC.life <= 0)
+            {
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, Vector2.Zero, Mod.Find<ModGore>("GrabberPlantGore1").Type, 1f);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, Vector2.Zero, Mod.Find<ModGore>("GrabberPlantGore2").Type, 1f);
+                Gore.NewGore(NPC.GetSource_Death(), NPC.position, Vector2.Zero, Mod.Find<ModGore>("GrabberPlantGore3").Type, 1f);
+            }
+        }
+
+        float State
+        {
+            get => NPC.ai[0];
+            set => NPC.ai[0] = value;
+        }
+        private Player trappedPlayer { get; set; }
+        public override void AI()
+        {
+            // When in idle
+            if(State == 0)
+            {
+                return; // Does nothing
+            }
+            else
+            {
+                // When player is grabbed they can move very slowly still
+                // and escape using teleportation items
+                if (trappedPlayer.Hitbox.Intersects(NPC.Hitbox) && !trappedPlayer.dead)
+                {
+                    trappedPlayer.velocity = Vector2.Zero;
+                }
+                else // If player dies/escapes, reset state
+                {
+                    State = 0;
+                }
+            }
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
+        {
+            // Change state to 1
+            State = 1;
+            trappedPlayer = target;
+
+            // Grab the player
+            if(target.Center != NPC.Center)
+            {
+                target.Center = NPC.Center;
+            }
+            
+            if(!target.HasBuff(BuffID.Poisoned))
+            {
+                target.AddBuff(BuffID.Poisoned, 300);
+            }
+        }
+
+        private int frame = 0;
+        public override void FindFrame(int frameHeight)
+        {
+            // When in idle
+            if(State == 0)
+            {
+                // Loop through the first four frames
+                if (NPC.frameCounter % 20 == 0)
+                {
+                    frame++;
+                    if (frame > 3)
+                    {
+                        frame = 0;
+                    }
+                }
+                NPC.frameCounter++;
+            }
+            else // Otherwise play the last three and stay on the last one
+            {
+                // Ensure that the grab will begin at frame 4 below by setting it to three first
+                if(frame < 3)
+                {
+                    frame = 3;
+                }
+
+                // Loop through the last frames until until 2nd last is reached
+                // After that loop the two last frames
+                if (NPC.frameCounter % 10 == 0)
+                {
+                    if(frame < 7)
+                    {
+                        frame++;
+                    }
+                    else if (frame == 7)
+                    {
+                        frame--;
+                    }
+                }
+                NPC.frameCounter++;
+            }
+
+            // Set the frame
+            NPC.frame.Y = frameHeight * frame;
+        }
+
+        // Draw the NPC over the player
+        public override void DrawBehind(int index)
+        {
+            Main.instance.DrawCacheNPCsOverPlayers.Add(index);
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				
+                // Sets the spawning conditions of this NPC that is listed in the bestiary.
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Jungle,
+				
+                // Sets the description of this NPC that is listed in the bestiary.
+				new FlavorTextBestiaryInfoElement("This plant will grab any traveller who touches it. Keep your distance.")
+            });
+        }
+    }
+}
---
+++ Content/Tiles/Banners/GrabberPlantBannerTile.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ModdingTutorial.Content.Tiles.Banners
+{
+    internal class GrabberPlantBannerTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            //TileID.Sets.SwaysInWindBasic[Type] = true; // TODO add better effect once TML supports banner movement, now it uses foliage sway...
+            TileObjectData.newTile.StyleHorizontal = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide | AnchorType.SolidBottom, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.StyleWrapLimit = 111;
+            TileObjectData.addTile(Type);
+            DustType = -1;
+
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(16, 110, 41), name); // Name will be just Banner like the vanilla ones (set in localisation)
+        }
+
+        // The banner buff:
+        public override void NearbyEffects(int i, int j, bool closer)
+        {
+            if (closer)
+            {
+                int type = ModContent.NPCType<NPCs.GrabberPlant>();
+                Main.SceneMetrics.hasBanner = true;
+                Main.SceneMetrics.NPCBannerBuff[type] = true;
+            }
+        }
+    }
+}
