cfbf0cee32adf4a802c93f666f01892d0f0ac4a8
JeseGamerHD <->
2023-10-14 10:43:16 +0300

    Added config toggle for ModifiedRecipes

--- Common/Systems/ModifiedRecipes.cs
+++ Common/Systems/ModifiedRecipes.cs
@@ -1,6 +1,6 @@
-﻿using ModdingTutorial.Content.Items.Placeables.Ores;
+﻿using ModdingTutorial.Common.Configs;
+using ModdingTutorial.Content.Items.Placeables.Ores;
 using Terraria;
-using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -11,39 +11,43 @@ namespace ModdingTutorial.Common.Systems
     {
         public override void PostAddRecipes()
         {
-            for(int i = 0; i < Recipe.numRecipes; i++)
+            // The player can disable these changes if they want to in the mod configs
+            if(ModContent.GetInstance<VanillaChangeToggle>().ModifiedRecipesToggle)
             {
-                Recipe recipe = Main.recipe[i];
+                for (int i = 0; i < Recipe.numRecipes; i++)
+                {
+                    Recipe recipe = Main.recipe[i];
 
-                // If recipe results in FrostHelmet and the recipe used TitaniumBar as an ingredient
-                if(recipe.HasResult(ItemID.FrostHelmet) && recipe.TryGetIngredient(ItemID.TitaniumBar, out Item titaniumBar))
-                {
-                    recipe.RemoveIngredient(titaniumBar); // Remove the ingredient
-                    recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 10); // Replace it
-                }
-                else if (recipe.HasResult(ItemID.FrostHelmet) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item adamantiteBar))
-                {
-                    recipe.DisableRecipe(); // Here just disable the alt recipe, this mod only has one recipe for this armor
-                }
+                    // If recipe results in FrostHelmet and the recipe used TitaniumBar as an ingredient
+                    if (recipe.HasResult(ItemID.FrostHelmet) && recipe.TryGetIngredient(ItemID.TitaniumBar, out Item titaniumBar))
+                    {
+                        recipe.RemoveIngredient(titaniumBar); // Remove the ingredient
+                        recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 10); // Replace it
+                    }
+                    else if (recipe.HasResult(ItemID.FrostHelmet) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item adamantiteBar))
+                    {
+                        recipe.DisableRecipe(); // Here just disable the alt recipe, this mod only has one recipe for this armor
+                    }
 
-                else if (recipe.HasResult(ItemID.FrostBreastplate) && recipe.TryGetIngredient(ItemID.TitaniumBar, out titaniumBar))
-                {
-                    recipe.RemoveIngredient(titaniumBar);
-                    recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 20);
-                }
-                else if (recipe.HasResult(ItemID.FrostBreastplate) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out adamantiteBar))
-                {
-                    recipe.DisableRecipe();
-                }
+                    else if (recipe.HasResult(ItemID.FrostBreastplate) && recipe.TryGetIngredient(ItemID.TitaniumBar, out titaniumBar))
+                    {
+                        recipe.RemoveIngredient(titaniumBar);
+                        recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 20);
+                    }
+                    else if (recipe.HasResult(ItemID.FrostBreastplate) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out adamantiteBar))
+                    {
+                        recipe.DisableRecipe();
+                    }
 
-                else if (recipe.HasResult(ItemID.FrostLeggings) && recipe.TryGetIngredient(ItemID.TitaniumBar, out titaniumBar))
-                {
-                    recipe.RemoveIngredient(titaniumBar);
-                    recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 16);
-                }
-                else if (recipe.HasResult(ItemID.FrostLeggings) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out adamantiteBar))
-                {
-                    recipe.DisableRecipe();
+                    else if (recipe.HasResult(ItemID.FrostLeggings) && recipe.TryGetIngredient(ItemID.TitaniumBar, out titaniumBar))
+                    {
+                        recipe.RemoveIngredient(titaniumBar);
+                        recipe.AddIngredient(ModContent.ItemType<FrostBar>(), 16);
+                    }
+                    else if (recipe.HasResult(ItemID.FrostLeggings) && recipe.TryGetIngredient(ItemID.AdamantiteBar, out adamantiteBar))
+                    {
+                        recipe.DisableRecipe();
+                    }
                 }
             }
         }
