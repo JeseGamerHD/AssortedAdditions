5c2c2f39899786be1ad2e0166000e37b662ab7f8
JeseGamerHD <->
2023-10-27 12:38:38 +0300

    Boss/Event summon item consumable toggle + code cleaning

--- Common/Configs/VanillaChangeToggle.cs
+++ Common/Configs/VanillaChangeToggle.cs
@@ -14,6 +14,15 @@ namespace ModdingTutorial.Common.Configs
         [ReloadRequired] // Marking it with [ReloadRequired] makes tModLoader force a mod reload if the option is changed. It should be used for things like item toggles, which only take effect during mod loading
         public bool ModifiedRecipesToggle;
 
+        [Header("Items")]
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool NoConsumeBossSummon;
+        
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool NoConsumeEventSummon;
+
         [Header("OreProgression")]
         [DefaultValue(true)]
         public bool HardmodeOreProgressionToggle;
---
+++ Common/GlobalItems/ReusableSummonItems.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using ModdingTutorial.Common.Configs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Common.GlobalItems
+{
+    // Majority of boss/event summon items are no longer consumables
+    internal class ReusableSummonItems : GlobalItem
+    {
+        // Only voodoo dolls, lacewing and truffle worm are used up since they function differently
+        private static readonly List<int> noConsumeBoss = new List<int>()
+        {
+            ItemID.Abeemination,
+            ItemID.BloodySpine,
+            ItemID.CelestialSigil,
+            ItemID.DeerThing,
+            ItemID.QueenSlimeCrystal,
+            ItemID.LihzahrdPowerCell,
+            ItemID.MechanicalEye,
+            ItemID.MechanicalSkull,
+            ItemID.MechanicalWorm,
+            ItemID.SlimeCrown,
+            ItemID.SuspiciousLookingEye,
+            ItemID.WormFood
+        };
+
+        private static readonly List<int> noConsumeEvent = new List<int>()
+        {
+            ItemID.BloodMoonStarter,
+            ItemID.GoblinBattleStandard,
+            ItemID.NaughtyPresent,
+            ItemID.PirateMap,
+            ItemID.PumpkinMoonMedallion,
+            ItemID.SnowGlobe,
+            ItemID.SolarTablet
+        };
+
+        public override void SetDefaults(Item entity)
+        {
+            // This can be toggled ON/OFF in the configs
+            if (ModContent.GetInstance<VanillaChangeToggle>().NoConsumeBossSummon)
+            {
+                if (noConsumeBoss.Contains(entity.type))
+                {
+                    entity.consumable = false;
+                }
+            }
+
+            if(ModContent.GetInstance<VanillaChangeToggle>().NoConsumeEventSummon)
+            {
+                if (noConsumeEvent.Contains(entity.type))
+                {
+                    entity.consumable = false;
+                }
+            }
+        }
+    }
+}
