4700e984c67399e779efc4f3a1d1f9de80ff64c1
JeseGamerHD <->
2023-11-03 18:44:43 +0200

    Added Bomb Launcher

---
+++ Content/Items/Weapons/Ranged/BombLauncher.cs
@@ -0,0 +1,54 @@
+﻿using ModdingTutorial.Content.Projectiles.RangedProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Items.Tools;
+
+namespace ModdingTutorial.Content.Items.Weapons.Ranged
+{
+    internal class BombLauncher : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 58;
+            Item.height = 32;
+            Item.damage = 36;
+            Item.knockBack = 8f;
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+            Item.shootSpeed = 11f;
+            Item.crit = 4;
+
+            Item.noMelee = true;
+            Item.autoReuse = true;
+
+            Item.value = Item.sellPrice(gold: 3);
+            Item.rare = ItemRarityID.Orange;
+            Item.UseSound = SoundID.Item61;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shoot = ModContent.ProjectileType<BombLauncherProj>();
+        }
+
+        public override Vector2? HoldoutOffset() => new(-10, 0); // Alligns the sprite properly
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset; // Projectiles come out of the muzzle properly using this
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.SnowballCannon)
+                .AddIngredient(ItemID.IllegalGunParts)
+                .AddIngredient(ModContent.ItemType<BagOfBombs>())
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/RangedProj/BombLauncherProj.cs
@@ -0,0 +1,85 @@
+﻿using Terraria.Audio;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ModdingTutorial.Content.Projectiles.RangedProj
+{
+    internal class BombLauncherProj : ModProjectile
+    {
+        public override string Texture => "ModdingTutorial/Content/Projectiles/NPCProj/GoblinBomb";
+        public override void SetDefaults()
+        {
+            Projectile.width = 22;
+            Projectile.height = 30;
+            Projectile.light = 0.1f;
+
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+
+            Projectile.DamageType = DamageClass.Ranged;
+
+            Projectile.aiStyle = -1;
+        }
+
+        public override void AI()
+        {
+            if(Projectile.direction == 1)
+            {
+                Projectile.rotation += 0.4f;
+            } 
+            else {
+                Projectile.rotation -= 0.4f;
+            }
+
+            Projectile.velocity.Y = Projectile.velocity.Y + 0.18f;
+            if (Projectile.velocity.Y > 16f)
+            {
+                Projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            int explosionArea = 80;
+            Vector2 oldSize = Projectile.Size;
+            // Resize the projectile hitbox to be bigger.
+            Projectile.position = Projectile.Center;
+            Projectile.Size += new Vector2(explosionArea);
+            Projectile.Center = Projectile.position;
+
+            Projectile.tileCollide = false;
+            Projectile.velocity *= 0.01f;
+            // Damage enemies inside the hitbox area
+            Projectile.Damage();
+
+            //Resize the hitbox to its original size
+            Projectile.position = Projectile.Center;
+            Projectile.Size = new Vector2(10);
+            Projectile.Center = Projectile.position;
+
+            // Sound effect
+            SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+
+            // Smoke Dust
+            for (int i = 0; i < 30; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 2f);
+                dust.velocity *= 1.2f;
+            }
+
+            // Fire Dust
+            for (int i = 0; i < 40; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3f);
+                dust.noGravity = true;
+                dust.velocity *= 5f;
+                dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 2f);
+                dust.velocity *= 3f;
+                dust.noGravity = true;
+            }
+        }
+    }
+}
