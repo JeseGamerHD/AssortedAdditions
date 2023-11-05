66507c30a89e923bccd59115812743a247546b77
JeseGamerHD <->
2023-11-05 11:44:22 +0200

    Added Spore Necklace

---
+++ Content/Items/Accessories/SporeNecklace.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using ModdingTutorial.Content.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Items.Accessories
+{
+    internal class SporeNecklace : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.value = Item.sellPrice(gold: 6);
+            Item.rare = ItemRarityID.Pink;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<SporeNecklacePlayer>().isWearingSporeNecklace = true;
+        }
+    }
+
+    internal class SporeNecklacePlayer : ModPlayer
+    {
+        public bool isWearingSporeNecklace;
+
+        public override void OnHurt(Player.HurtInfo info)
+        {
+            if(isWearingSporeNecklace && Player.whoAmI == Main.myPlayer)
+            {
+                if (Player.ownedProjectileCounts[ModContent.ProjectileType<SporeNecklaceProj>()] < 25)
+                {
+                    // Spawn 5 projectiles with random upwards velocity
+                    for(int i = 0; i <= 5; i++)
+                    {
+                        Vector2 launchVelocity = new Vector2(Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-4, -2));
+                        Projectile.NewProjectile(Player.GetSource_FromThis(), Player.Center, launchVelocity, ModContent.ProjectileType<SporeNecklaceProj>(), 45, 2, Main.myPlayer);
+                    }
+                }
+            }
+        }
+
+        public override void ResetEffects()
+        {
+            isWearingSporeNecklace = false;
+        }
+    }
+}
---
+++ Content/Projectiles/SporeNecklaceProj.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ModdingTutorial.Content.Projectiles
+{
+    internal class SporeNecklaceProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 14;
+            Projectile.timeLeft = 1800;
+            Projectile.light = 0.25f;
+            Projectile.penetrate = 6;
+
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+
+            Projectile.aiStyle = ProjAIStyleID.GroundProjectile;
+            AIType = ProjectileID.SpikyBall;
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            fallThrough = false;
+            return true;
+        }
+
+        public override void PostAI()
+        {
+            if(Projectile.timeLeft < 60)
+            {
+                Projectile.alpha += 5;
+            }
+        }
+    }
+}
