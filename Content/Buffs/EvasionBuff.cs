83c41c42b99f003be53770c74e10cfe505a31924
JeseGamerHD <->
2023-12-14 15:16:54 +0200

    Added Evasion Potion

---
+++ Content/Buffs/EvasionBuff.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedAdditions.Content.Buffs
+{
+	internal class EvasionBuff : ModBuff
+	{
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.GetModPlayer<EvasionBuffPlayer>().hasEvasionBuff = true;
+		}
+	}
+
+	internal class EvasionBuffPlayer : ModPlayer 
+	{ 
+		public bool hasEvasionBuff = false;
+
+		public override bool FreeDodge(Player.HurtInfo info)
+		{
+			if(hasEvasionBuff)
+			{
+				if (Main.rand.NextBool(20))
+				{
+					Player.immune = true;
+					Player.immuneTime = 75;
+					Player.immuneNoBlink = false;
+
+					for(int i = 0; i < 30; i++)
+					{
+						Dust dust = Dust.NewDustDirect(Player.position, Player.width, Player.height, DustID.RainCloud);
+						dust.noGravity = true;
+						dust.velocity *= 1.2f;
+					}
+
+					NetMessage.SendData(MessageID.Dodge, -1, -1, null, Player.whoAmI, 1f, 0f, 0f, 0, 0, 0);
+
+					return true;
+				}
+			}
+
+
+			return base.FreeDodge(info);
+		}
+
+		public override void ResetEffects()
+		{
+			hasEvasionBuff = false;
+
+		}
+	}
+}
---
+++ Content/Items/Consumables/EvasionPotion.cs
@@ -0,0 +1,52 @@
+﻿using AssortedAdditions.Content.Buffs;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace AssortedAdditions.Content.Items.Consumables
+{
+	internal class EvasionPotion : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 20;
+
+			// Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+			ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+				new Color(171, 19, 9),
+				new Color(196, 95, 88),
+				new Color(82, 18, 14)
+			};
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 26;
+			Item.height = 32;
+			Item.useAnimation = 15;
+			Item.useTime = 15;
+			Item.maxStack = 9999;
+
+			Item.consumable = true;
+			Item.useTurn = true;
+
+			Item.useStyle = ItemUseStyleID.DrinkLiquid;
+			Item.UseSound = SoundID.Item3;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 6);
+			Item.buffType = ModContent.BuffType<EvasionBuff>();
+			Item.buffTime = 10800; // 3 minutes
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.SwiftnessPotion);
+			recipe.AddIngredient(ItemID.Bone, 5);
+			recipe.AddIngredient(ItemID.GlowingMushroom);
+			recipe.AddTile(TileID.Bottles);
+			recipe.Register();
+		}
+	}
+}
