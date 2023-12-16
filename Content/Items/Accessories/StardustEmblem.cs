e013fe1abb00e08bd03759e83adedbc697390ce3
JeseGamerHD <->
2023-12-16 11:30:22 +0200

    Added lunar emblems

---
+++ Content/Items/Accessories/StardustEmblem.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AssortedAdditions.Content.Items.Accessories
+{
+	internal class StardustEmblem : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.width = 32;
+			Item.height = 32;
+			Item.maxStack = 1;
+			Item.value = Item.sellPrice(gold: 10);
+			Item.rare = ItemRarityID.Red;
+			Item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.GetDamage(DamageClass.Summon) += 0.2f;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.AvengerEmblem);
+			recipe.AddIngredient(ItemID.FragmentStardust, 10);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.Register();
+		}
+	}
+}
---
+++ Content/Items/Accessories/VortexEmblem.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AssortedAdditions.Content.Items.Accessories
+{
+	internal class VortexEmblem : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.width = 32;
+			Item.height = 32;
+			Item.maxStack = 1;
+			Item.value = Item.sellPrice(gold: 10);
+			Item.rare = ItemRarityID.Red;
+			Item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.GetDamage(DamageClass.Ranged) += 0.2f;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.AvengerEmblem);
+			recipe.AddIngredient(ItemID.FragmentVortex, 10);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.Register();
+		}
+	}
+}
