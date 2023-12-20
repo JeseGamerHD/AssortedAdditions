50ca2f56d38b136cd13f51bbf5286488e0566290
JeseGamerHD <->
2023-12-20 11:26:58 +0200

    Runes cant have prefixes/modifiers

--- Content/Items/Accessories/Runes/RuneItem.cs
+++ Content/Items/Accessories/Runes/RuneItem.cs
@@ -1,10 +1,12 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
+using Terraria.Utilities;
 
 namespace AssortedAdditions.Content.Items.Accessories.Runes
 {
 	/// <summary>
-	/// Base class for rune accessories. Sets CanEquipAccessory to return true for modded slots.
+	/// Base class for rune accessories. CanEquipAccessory returns true only for modded slots.
+	/// Disables prefixes (modifiers/forging) of these items.
 	/// Other use is to check if the item is a RuneItem type in RuneSlot.cs
 	/// </summary>
 	public abstract class RuneItem : ModItem 
@@ -12,6 +14,11 @@ namespace AssortedAdditions.Content.Items.Accessories.Runes
 		public override bool CanEquipAccessory(Player player, int slot, bool modded)
 		{
 			return modded; // Can only be equipped in the rune slot
+		}
+
+		public override bool? PrefixChance(int pre, UnifiedRandom rand)
+		{
+			return false;
 		}
 	}
 }
