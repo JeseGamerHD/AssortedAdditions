01057a19fbe95924f719fbdfd7ee7a95ed4bfdf0
JeseGamerHD <->
2023-12-26 13:42:43 +0200

    Projectile counter helper

---
+++ Helpers/HelperMethods.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+
+namespace AssortedAdditions.Helpers
+{
+	public class HelperMethods
+	{
+		/// <summary>
+		/// This method should be used when ownedProjectileCounts does not work properly (it has a one tick delay).
+		/// Loops through Main.projectile[] and counts how many projectiles of given type exist. Owner should be player.whoAmI, stopWhenFoundOne = true if only need to check if one exists.
+		/// </summary>
+		public static int CountProjectiles(int type, int owner, bool stopWhenFoundOne = false)
+		{
+			int num = 0;
+
+			for(int i = 0; i < Main.maxProjectiles; i++)
+			{
+				if (Main.projectile[i].type == type && Main.projectile[i].active && Main.projectile[i].owner == owner)
+				{
+					num++;
+
+					if (stopWhenFoundOne)
+					{
+						break;
+					}
+				}
+			}
+
+			return num;
+		}
+	}
+}
