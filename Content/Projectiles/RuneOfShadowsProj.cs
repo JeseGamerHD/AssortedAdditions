234ba0f9b10bda45f7ce6a51ebf1d9675963046e
JeseGamerHD <->
2023-12-27 16:29:23 +0200

    Forgot cooldown

--- Content/Projectiles/RuneOfShadowsProj.cs
+++ Content/Projectiles/RuneOfShadowsProj.cs
@@ -173,7 +173,7 @@ namespace AssortedAdditions.Content.Projectiles
 		{
 			Player player = Main.player[Projectile.owner];
 			player.GetModPlayer<RuneOfShadowsPlayer>().canSpawnProjectile = true; // Allow the projectiles to respawn if the player respawns after dying or re-equips the rune
-			player.AddBuff(ModContent.BuffType<RuneOfShadowsCooldown>(), 180); // TODO increase to 3 minutes once done
+			player.AddBuff(ModContent.BuffType<RuneOfShadowsCooldown>(), HelperMethods.MinutesToTicks(3));
 
 			// Only one can be spawned
 			if (HelperMethods.CountProjectiles(ModContent.ProjectileType<RuneOfShadowsAbilityControl>(), player.whoAmI, true) == 0) 
--- Helpers/HelperMethods.cs
+++ Helpers/HelperMethods.cs
@@ -5,6 +5,22 @@ namespace AssortedAdditions.Helpers
 	public class HelperMethods
 	{
 		/// <summary>
+		/// Converts seconds to ticks
+		/// </summary>
+		public static int SecondsToTicks(int seconds)
+		{
+			return seconds * 60;
+		}
+
+		/// <summary>
+		/// Converts minutes to ticks
+		/// </summary>
+		public static int MinutesToTicks(int minutes)
+		{
+			return SecondsToTicks(minutes) * 60;
+		}
+
+		/// <summary>
 		/// This method should be used when ownedProjectileCounts does not work properly (it has a one tick delay).
 		/// Loops through Main.projectile[] and counts how many projectiles of given type exist. Owner should be player.whoAmI, stopWhenFoundOne = true if only need to check if one exists.
 		/// </summary>
