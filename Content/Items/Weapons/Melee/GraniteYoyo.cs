2d160aab8159c2a6321ea681459a2d50db774830
JeseGamerHD <->
2024-01-14 11:08:28 +0200

    Granite Yoyo

---
+++ Content/Items/Weapons/Melee/GraniteYoyo.cs
@@ -0,0 +1,51 @@
+﻿using AssortedAdditions.Content.Projectiles.MeleeProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace AssortedAdditions.Content.Items.Weapons.Melee
+{
+	internal class GraniteYoyo : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			ItemID.Sets.Yoyo[Item.type] = true;
+			ItemID.Sets.GamepadExtraRange[Item.type] = 20;
+			ItemID.Sets.GamepadSmartQuickReach[Item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 26;
+			Item.useTime = 25; // Vanilla yoyo's have this at 25
+			Item.useAnimation = 25; // ^^
+			Item.damage = 15;
+			Item.knockBack = 5f;
+			Item.crit = 2;
+			Item.shootSpeed = 16f;
+
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+			Item.channel = true;
+
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.DamageType = DamageClass.MeleeNoSpeed;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item1;
+			Item.value = Item.sellPrice(silver: 80);
+			Item.shoot = ModContent.ProjectileType<GraniteYoyoProj>();
+		}
+	}
+
+	public class GraniteYoyoPlayer : ModPlayer
+	{
+		public override void UpdateEquips()
+		{
+			if(Player.HeldItem.ModItem is GraniteYoyo)
+			{
+				Player.stringColor = PaintID.DeepPurplePaint;
+			}
+		}
+	}
+}
---
+++ Content/Projectiles/MeleeProj/GraniteYoyoProj.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedAdditions.Content.Projectiles.MeleeProj
+{
+	internal class GraniteYoyoProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 7f;
+			ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 128; // Divide the value by 16 to get the reach in tiles
+			ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 13f;
+
+			ProjectileID.Sets.TrailCacheLength[Type] = 20;
+			ProjectileID.Sets.TrailingMode[Type] = 3;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 16;
+			Projectile.height = 16;
+			Projectile.penetrate = -1;
+
+			Projectile.friendly = true;
+
+			Projectile.aiStyle = ProjAIStyleID.Yoyo;
+			Projectile.DamageType = DamageClass.MeleeNoSpeed;
+		}
+
+		public override void PostAI()
+		{
+			if (Main.rand.NextBool(4))
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width * 2, Projectile.height * 2, DustID.Granite);
+				dust.noGravity = true;
+				dust.velocity *= 1.5f;
+			}
+
+			Lighting.AddLight((int)Projectile.position.X / 16, (int)Projectile.position.Y / 16, TorchID.Purple, 0.25f);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			VertexStrip vertexStrip = new();
+			MiscShaderData miscShaderData = GameShaders.Misc["LightDisc"];
+			miscShaderData.UseSaturation(-2.8f);
+			miscShaderData.UseOpacity(4f);
+			miscShaderData.Apply();
+			vertexStrip.PrepareStripWithProceduralPadding(Projectile.oldPos, Projectile.oldRot, StripColors, StripWidth, -Main.screenPosition + Projectile.Size / 2, true);
+			vertexStrip.DrawTrail();
+			Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+
+			return true;
+		}
+
+		private Color StripColors(float progressOnStrip)
+		{
+			Color result = Color.Lerp(Color.MidnightBlue, Color.MidnightBlue, Utils.GetLerpValue(-0.2f, 0.5f, progressOnStrip, clamped: false)) * (1f - Utils.GetLerpValue(0f, 0.98f, progressOnStrip));
+			result.A = 0;
+			return result;
+		}
+
+		private float StripWidth(float progressOnStrip)
+		{
+			float num = 0.1f;
+			float lerpValue = Utils.GetLerpValue(0f, 1f, progressOnStrip, clamped: true);
+			num *= 1f - (1f - lerpValue) * (1f - lerpValue);
+			return MathHelper.Lerp(0f, 32f, num);
+		}
+	}
+}
