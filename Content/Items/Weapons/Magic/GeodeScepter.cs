c8a4642aea3d41df79751003787450959af9d31b
JeseGamerHD <->
2024-01-17 15:56:07 +0200

    Geode Scepter

---
+++ Content/Items/Weapons/Magic/GeodeScepter.cs
@@ -0,0 +1,74 @@
+﻿using AssortedAdditions.Content.Projectiles.MagicProj;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent;
+using Terraria.DataStructures;
+
+namespace AssortedAdditions.Content.Items.Weapons.Magic
+{
+	internal class GeodeScepter : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.staff[Type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 29;
+			Item.height = 28;
+			Item.damage = 13;
+			Item.knockBack = 6f;
+			Item.useAnimation = 30;
+			Item.useTime = 30;
+			Item.shootSpeed = 10f;
+			Item.mana = 8;
+			Item.scale = 1.75f;
+
+			Item.autoReuse = true;
+			Item.noMelee = true;
+
+			Item.value = Item.sellPrice(silver: 70);
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.DamageType = DamageClass.Magic;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item88;
+			Item.shoot = ModContent.ProjectileType<GeodeScepterProj>();
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			float numberProjectiles = 3;
+			float rotation = MathHelper.ToRadians(10);
+
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1)));
+				Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+			}
+
+			return false;
+		}
+
+		// Draw item with increased scale when dropped
+		public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+		{
+			Texture2D texture = TextureAssets.Item[Type].Value; // Item's spritesheet
+
+			Rectangle? source = null;
+			if (Main.itemAnimations[Type] != null)
+			{
+				// The current frame of the animation, null check for items that have one frame
+				source = Main.itemAnimations[Type].GetFrame(texture);
+			}
+
+			Main.spriteBatch.Draw(texture, new Vector2(Item.position.X, Item.position.Y - 17) - Main.screenPosition, 
+				source, lightColor, 0, Vector2.Zero, scale * 1.5f, SpriteEffects.None, 0f);
+
+			return false;
+		}
+	}
+}
---
+++ Content/Projectiles/MagicProj/GeodeScepterProj.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AssortedAdditions.Content.Projectiles.MagicProj
+{
+	internal class GeodeScepterProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.projFrames[Projectile.type] = 6;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 16;
+			Projectile.height = 20;
+			Projectile.timeLeft = 300;
+
+			Projectile.DamageType = DamageClass.Magic;
+
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.friendly = true;
+		}
+
+		private bool setOnce = false;
+		private readonly int [] dustTrailType = { DustID.GemDiamond, DustID.GemSapphire, DustID.GemTopaz, DustID.GemAmethyst, DustID.GemRuby, DustID.GemEmerald };
+
+		public override void AI()
+		{
+			if(!setOnce)
+			{
+				Projectile.frame = Main.rand.Next(Main.projFrames[Projectile.type]);
+				setOnce = true;
+			}
+
+			if (Main.rand.NextBool())
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position - Projectile.velocity, Projectile.width, Projectile.height, dustTrailType[Projectile.frame]);
+				dust.noGravity = true;
+			}
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height); // Dust from tile when hit
+			SoundEngine.PlaySound(SoundID.Dig, Projectile.Center); // Tile hit sound
+			return true;
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position - Projectile.velocity, Projectile.width, Projectile.height,
+					dustTrailType[Projectile.frame], 0, 0, 150, default, 1f);
+				dust.noGravity = true;
+			}
+		}
+	}
+}
