8179e2cb01cfda87b6b484273392f307c490e160
JeseGamerHD <->
2024-03-09 17:20:25 +0200

    Sprite update + cleanup

--- Content/Items/Weapons/Ammo/GuidedMissile.cs
+++ Content/Items/Weapons/Ammo/GuidedMissile.cs
@@ -7,24 +7,21 @@ namespace AssortedAdditions.Content.Items.Weapons.Ammo
 {
     internal class GuidedMissile : ModItem
     {
-        public override void SetStaticDefaults()
-        {
-            // Tooltip.SetDefault("Small blast radius. Will not destroy tiles");
-        }
         public override void SetDefaults()
         {
             Item.width = 32;
             Item.height = 32;
             Item.damage = 20;
-            Item.maxStack = 999;
+            Item.maxStack = 9999;
+            Item.knockBack = 4f;
+
+            Item.consumable = true;
+
             Item.value = Item.sellPrice(0, 0, 2, 0);
             Item.DamageType = DamageClass.Ranged;
-            Item.consumable = true;
             Item.value = Item.buyPrice(silver: 1);
             Item.rare = ItemRarityID.Blue;
-            Item.knockBack = 4f;
             Item.shoot = ModContent.ProjectileType<GuidedMissileProj>();
-
             Item.ammo = Item.type;
         }
     }
--- Content/Items/Weapons/Ranged/GuidedMissileLauncher.cs
+++ Content/Items/Weapons/Ranged/GuidedMissileLauncher.cs
@@ -11,55 +11,38 @@ namespace AssortedAdditions.Content.Items.Weapons.Ranged;
 
 internal class GuidedMissileLauncher : ModItem
 {
-    public override void SetStaticDefaults()
-    {
-        // Tooltip.SetDefault("Launches controllable missiles");
-    }
-
     public override void SetDefaults()
     {
-        Item.damage = 65;
+		Item.width = 56;
+        Item.height = 28;
+		Item.useTime = 20;
+		Item.useAnimation = 20;
+		Item.damage = 65;
         Item.knockBack = 4;
-        Item.DamageType = DamageClass.Ranged;
-
-        Item.width = 20;
-        Item.height = 20;
-        Item.channel = true;
-
-        Item.useTime = 20;
-        Item.useAnimation = 20;
-
-        Item.useStyle = ItemUseStyleID.Shoot;
-        Item.noMelee = true;
-
-        Item.value = Item.buyPrice(gold: 80);
-        Item.rare = ItemRarityID.Yellow;
-
-        Item.autoReuse = true;
-
         Item.shootSpeed = 10f;
+
+        Item.channel = true;
+		Item.autoReuse = true;
+		Item.noMelee = true;
+
+		Item.useStyle = ItemUseStyleID.Shoot;
+		Item.DamageType = DamageClass.Ranged;
+		Item.value = Item.buyPrice(gold: 80);
+        Item.rare = ItemRarityID.Yellow;
         Item.shoot = ModContent.ProjectileType<GuidedMissileProj>();
         Item.useAmmo = ModContent.ItemType<GuidedMissile>();
-
         Item.UseSound = SoundID.Item10;
     }
-    public override Vector2? HoldoutOffset() => new(-10, 2); // Alligns the sprite properly
+    public override Vector2? HoldoutOffset() => new(-20, -5); // Alligns the sprite properly
 
-    // Needed so that the projectile shoots from the muzzle
     public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
     {
-        Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
-        if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
-        {
-            if (player.direction == -1) // If the player is facing left
-            {
-                muzzleOffset.Y += 15; // Adjust the offset
-                muzzleOffset.X -= 20;
-            }
-
-            position += muzzleOffset;
-        }
-    }
+		Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+		if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+		{
+			position += muzzleOffset; // Projectiles come out of the muzzle properly using this
+		}
+	}
     public override void AddRecipes()
     {
         Recipe recipe = CreateRecipe();
