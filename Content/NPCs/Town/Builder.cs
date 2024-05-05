edf1a7c1abc4a4493549194b2631969434cfa31f
JeseGamerHD <->
2024-05-05 16:39:13 +0300

    github verify pls...

--- Content/NPCs/Town/Builder.cs
+++ Content/NPCs/Town/Builder.cs
@@ -209,7 +209,7 @@ namespace AssortedAdditions.Content.NPCs.Town
 
         public override void TownNPCAttackStrength(ref int damage, ref float knockback)
         {
-            damage = 15;
+            damage = 16;
             knockback = 2f;
         }
 
